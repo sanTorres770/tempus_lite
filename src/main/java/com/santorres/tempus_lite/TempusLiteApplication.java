29c5f06bd7ad493527d10f78992bde3298bc25c8
sanTorres770 <sanpez-1994@hotmail.com>
2023-05-28 18:29:38 -0500

    se agrega estructura del proyecto

---
+++ src/main/java/com/santorres/tempus_lite/TempusLiteApplication.java
@@ -0,0 +1,13 @@
+package com.santorres.tempus_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempusLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempusLiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santorres/tempus_lite/TempusLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santorres.tempus_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempusLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
